144ba96f69913f0a825319cc810807ccb58ad355
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-17 15:01:17 -0400

    Layout preview component created to sandboxx a theme preview template system that can be customized to the users needs.

--- src/components/layoutPreview.jsx
+++ src/components/layoutPreview.jsx
@@ -1,2 +1,44 @@
 /* //themedevstool/src/components/layoutPreview.jsx */
 
+function LayoutPreview() {
+    return (
+        <div className="layout-preview">
+
+            <nav className="navbar">
+                Theme Dev Tool
+            </nav>
+
+            <header className="hero">
+                <h1>Welcome</h1>
+                <p>THis is a theme page preview</p>
+            </header>
+
+            <section className="section">
+
+                <h2>Cards</h2>
+
+                <div className="card-grid">
+                    <div className="card">
+                        Example Card
+                    </div>
+
+                    <div className="card">
+                        Example Card
+                    </div>
+
+                    <div className="card">
+                        Example Card
+                    </div>
+
+                </div>
+
+            </section>
+
+            <footer className="footer">
+            Footer Content
+            </footer>
+        </div>
+    )
+}
+
+export default LayoutPreview
