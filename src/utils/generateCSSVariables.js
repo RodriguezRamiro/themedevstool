5bb6190cc73e9b4fbd8e97389a4fc116c6e863e0
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-18 20:08:52 -0400

    now css variables are exported in the theme. automatically adpat when new tokens are added, naming are clean and predictable.

--- src/utils/generateCSSVariables.js
+++ src/utils/generateCSSVariables.js
@@ -1,25 +1,20 @@
 /* //themedevstool/src/utils/generateCSSVariables.js */
 
+
 export function generateCSSVariables(theme) {
+    const createVars = (obj) => {
+      return Object.entries(obj)
+        .map(([key, value]) => `--${key}: ${value};`)
+        .join("\n");
+    };
+
     return `
-    :root {
-        --color-primary: ${theme.light.primary};
-        --color-secondary: ${theme.light.secondary};
-        --color-accent: ${theme.light.accent};
+  :root {
+  ${createVars(theme.light)}
+  }
 
-        --color-background: ${theme.light.background};
-        --color-surface: ${theme.light.surface};
-        --color-text: ${theme.light.text};
-    }
-
-    [data-theme="dark"] {
-        --color-primary: ${theme.dark.primary};
-        --color-secondary: ${theme.dark.secondary};
-        --color-accent: ${theme.dark.accent};
-
-        --color-background: ${theme.dark.background};
-        --color-surface: ${theme.dark.surface};
-        --color-text: ${theme.dark.text};
-    }
-    `
-}
+  [data-theme="dark"] {
+  ${createVars(theme.dark)}
+  }
+  `;
+  }
