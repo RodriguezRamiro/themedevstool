f0dd2186b7e077038f2a85109d242ac5da1649e4
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-22 07:29:42 -0400

    fixed warning logic bug moved warning logs inside function placed return after warning.

--- src/utils/applytheme.js
+++ src/utils/applytheme.js
@@ -1,13 +1,17 @@
 /* //themedevstool/src/utils/applyTheme.js */
 
 export function applyThemeToDocument(theme, mode = "light") {
-    if (!theme) return;
-      console.warn("No theme provided");
+  if (!theme) {
+    console.warn("No theme provided");
+    return;
+  }
 
     const colors = theme[mode];
-    if (!colors) return;
-      console.warn("Invalid mode:", mode);
 
+    if (!colors)
+      console.warn("Invalid mode:", mode);
+      return;
+}
     const root = document.documentElement;
 
     // Set theme attribute
