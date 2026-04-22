9aa2b9f3e238814892581890b109139999e9d19b
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-22 08:05:27 -0400

    system working correclty thmees generate but dont apply to the page... changed hues lightness and saturation to create more dramatic visual shifts

--- src/utils/generatePalette.js
+++ src/utils/generatePalette.js
@@ -1,20 +1,26 @@
 /* //themedevstool/src/utils/generatePalette.js */
 
 function randomHue() {
-    return Math.floor(Math.random() * 360)
-}
+    return Math.floor(Math.random() * 360);
+  }
 
-function shiftHue(base, shift) {
-     return (base + shift) % 360;
-}
+  function shiftHue(base, shift) {
+    return (base + shift) % 360;
+  }
 
-export function generatePalette() {
-    const hue = randomHue()
+  function hsl(h, s, l) {
+    return `hsl(${h}, ${s}, ${l}%)`;
+  }
+
+  export function generatePalette() {
+    const hue = randomHue();
+
+    console.log("Base hue:", hue);
 
     return [
-        hsl(hue, 70, 50),     // primary
-        hsl(shiftHue(hue + 30), 65, 45), // secondary (slightly darker)
-        hsl(shiftHue(hue + 6), 80, 60), // accent (brighter)
+      hsl(hue, 80, 50),               // primary
+      hsl(shiftHue(hue, 30), 70, 35), // secondary
+      hsl(shiftHue(hue, 60), 90, 65), // accent
     ];
-
-}
+    
+  }
