6abf3f994020c47410845858ac09f3712454705f
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-23 14:09:47 -0400

    mode used inside applythemetodocument but not passed into funciton signature correclty. placed mode inside function to pass paramater accordingly.

--- src/utils/applytheme.js
+++ src/utils/applytheme.js
@@ -1,14 +1,14 @@
 /* //themedevstool/src/utils/applyTheme.js */
 
-export function applyThemeToDocument(theme) {
-  if (!theme) {
-    console.warn("No theme provided");
+export function applyThemeToDocument(theme, mode = "dark") {
+  if (!theme || !theme[mode]) {
+    console.warn("Invalid theme or mode");
     return;
   }
 
   const root = document.documentElement;
 
-  Object.entries(theme).forEach(([key, value]) => {
+  Object.entries(theme[mode]).forEach(([key, value]) => {
     root.style.setProperty(key, value);
   });
 }
