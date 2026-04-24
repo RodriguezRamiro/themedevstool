76ebcf2b30804e14e7fd0407b08edc8c6345b74d
Ramiro Rodriguez <rodriguezcodesolutions@gmail.com>
2026-04-24 14:10:07 -0400

    updated compoonents to mathc new architecture with all new variables

--- src/components/themePreview.jsx
+++ src/components/themePreview.jsx
@@ -1,16 +1,22 @@
 /* //themedevstool/src/components/themePreview.jsx */
 
-function ThemePreview({ theme }) {
+function ThemePreview({ theme, mode = "light" }) {
+
+    const currentTheme = theme?.[mode];
+
+    if (currentTheme) return null;
+
     return (
         <div>
 
-            <h2>Preview</h2>
+            <h2>Theme Preview</h2>
 
             <div className="swatches">
 
-                <div className="swatch" style={{ background: theme.light.primary }}>Primary</div>
-                <div className="swatch" style={{ background: theme.light.secondary }}>Secondary</div>
-                <div className="swatch" style={{ background: theme.light.accent }}>Accent</div>
+                <div className="swatch" style={{ background: currentTheme['--accent'] }}>Accent</div>
+                <div className="swatch" style={{ background: currentTheme['--accent-secondary'] }}>Accent Secondary</div>
+                <div className="swatch" style={{ background: currentTheme['--Surface'] }}>Surface</div>
+                <div className="swatch" style={{ background: currentTheme['--bg'] }}>Background</div>
 
             </div>
 
